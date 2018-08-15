060447221ee5f0b5ba6059861d1dd6a79c12759a
talal tahir <ttahir@billtrust.com>
2018-08-15 16:54:54 -0400

    Adding Geolocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+import { Geolocation } from '@ionic-native/geolocation';
 import {AgmCoreModule} from '@agm/core';
 
 import { MyApp } from './app.component';
@@ -31,7 +32,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,31 +1,54 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  ZOOM_LEVEL = 15;
   public map: any = {};
   public labelOptions = {
-    color: 'red',
+    color: '000000',
     fontFamily: '',
     fontSize: '18px',
     fontWeight: 'bold',
-    text: ' ',
+    text: 'tests ',
   }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {}
+
+  ionViewDidLoad(){
+    this.getUserLocation();
+  }
+
+
+  getUserLocation() {
+    console.log('getUserLocation() start');
+    //let GPSoptions = {timeout: 100000, enableHighAccuracy: false, maximumAge: 0};
+    let GPSoptions = { enableHighAccuracy: true, maximumAge: 0 };
+
+    this.geolocation.getCurrentPosition(GPSoptions)
+      .then((position) => {
+        this.handleResponse(position)
+      })
+      .catch((error) => {
+        console.log('Error getting location', error);
+      },
+
+    );
+    console.log('getUserLocation() end');
+
+  }
+
+
+  handleResponse(pos){
     this.map = {
-      lat: 40.283927299999995,
-      lng: -74.7044311,
-      zoom: 10
+      lat: pos.coords.latitude,
+      lng: pos.coords.longitude,
+      zoom: this.ZOOM_LEVEL
     }
-  }
-
-  ionViewDidLoad()
-  {
     
   }
 
